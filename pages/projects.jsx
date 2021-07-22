f35538699974af0b85882ac762f3314c48e5216c
Christian Paranas <christiannparanas@gmail.com>
2021-07-22 12:56:23 +0800

    proj heads

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -15,9 +15,27 @@ export default function Home() {
 
       <main className="projects">
         <div className="content">
-          Projects
-        </div>
+          <h1 className="title">Projects</h1>
+          <p className="head_p">Some existing open-source projects that I worked on with other people for work, or as personal side-projects.</p>
         
+          <div className="diff_projs">
+            <div className="diff_proj">
+              <div className="head">
+                <img draggable="false" role="img" className="emoji" alt="💼" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f4bc.svg" />
+                Work projects:</div>
+            </div>
+            <div className="diff_proj">
+              <div className="head">
+                <img draggable="false" role="img" className="emoji" alt="💻" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f4bb.svg" />
+                Personal projects:</div>
+            </div>
+            <div className="diff_proj">
+              <div className="head">
+                <img draggable="false" role="img" className="emoji" alt="🕰" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f570.svg" />
+                Older projects:</div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   )

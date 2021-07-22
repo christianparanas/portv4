e619c670bcaf01417ab89bdfe0c5637fb793b4ba
Christian Paranas <christiannparanas@gmail.com>
2021-07-22 20:37:11 +0800

    projects

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,10 +1,27 @@
 import Head from 'next/head'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
+// data
+import projects from './api/projects.json'
 
 
 export default function Home() {
+  const [projs, setProjs] = useState(projects)
+
+  useEffect(() => {
+    console.log(projects)
+
+  }, [])
+
+  if(!process.browser) return null
+
+
+  // external routing
+  const externalLink = (path) => {
+    window.location.href = path
+  }
 
   return (
     <div className="container">
@@ -22,17 +39,29 @@ export default function Home() {
             <div className="diff_proj">
               <div className="head">
                 <img draggable="false" role="img" className="emoji" alt="💼" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f4bc.svg" />
-                Work projects:</div>
+                Work projects:
+              </div>
             </div>
             <div className="diff_proj">
               <div className="head">
                 <img draggable="false" role="img" className="emoji" alt="💻" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f4bb.svg" />
-                Personal projects:</div>
+                Personal projects:
+              </div>
+              <div className="diff_proj_content">
+                <ul>
+                  {projs[1].map((val, key) => {
+                    return (
+                      <li key={key}><a href={val.projectLink}>{val.projectName}</a></li>
+                    )
+                  })}
+                </ul>
+              </div>
             </div>
             <div className="diff_proj">
               <div className="head">
                 <img draggable="false" role="img" className="emoji" alt="🕰" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f570.svg" />
-                Older projects:</div>
+                Older projects:
+              </div>
             </div>
           </div>
         </div>

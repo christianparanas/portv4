93a43b18657ab74f4e7633598fbd9c7b5e4a72cc
Yuri Orlov <christiannparanas@gmail.com>
2021-07-26 01:12:16 +0800

    Typo

--- pages/index.jsx
+++ pages/index.jsx
@@ -30,7 +30,7 @@ export default function Home() {
           </div>
           <div className="text">
             <div className="t1">
-              <img draggable="false" role="img" className="emoji" alt="🙎🏻‍♂️" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f64e-1f3fb-200d-2642-fe0f.svg" /> I’m a software developer currently in Tacloban city, <img draggable="false" role="img" className="emoji" alt="🇵🇭" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f1f5-1f1ed.svg" /> Philippines. 
+              <img draggable="false" role="img" className="emoji" alt="🙎🏻‍♂️" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f64e-1f3fb-200d-2642-fe0f.svg" /> I’m a software developer currently in Tacloban City, <img draggable="false" role="img" className="emoji" alt="🇵🇭" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f1f5-1f1ed.svg" /> Philippines. 
                Specializing in building websites and applications. My current toolset includes 
                Vue, React, Redux, Node, Laravel, and other various frameworks, libraries, and technologies related to them.
             </div>

a36e57a3281ef6f034493cfb566f229cd5625886
kate <christiannparanas@gmail.com>
2022-03-17 18:53:33 +0800

    fix: prod err

--- components/index.js
+++ components/index.js
@@ -2,4 +2,3 @@ export { default as Card } from "./Card";
 export { default as Nav } from "./Nav";
 export { default as Layout } from "./layout";
 export { default as ThemeChanger } from "./ThemeChanger";
-export { default as Code } from "./Code";

5e5ff1c46071cfb140ca8c05d0450d8d80e30f0e
kate <christiannparanas@gmail.com>
2022-03-21 18:30:35 +0800

    style: rm nl

--- pages/_app.js
+++ pages/_app.js
@@ -8,56 +8,27 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-        <title> Christian Paranas | web Developer </title>
-        <meta
-          name='description'
-          content='Hi, I am Christian Paranas a web development enthusiast that is armed with experience, learning, and passion.'
-        />
-        <meta
-          name='thumbnail'
-          content='https://christianparanas.vercel.app/me.png'
-        />
-        <meta
-          property='og:title'
-          content='Christian Paranas | Web Developer'
-        />
+        <title>Christian Paranas | web Developer</title>
+        <meta name="description" content="Hi, I am Christian Paranas a web development enthusiast that is armed with experience, learning, and passion." />
+        <meta name='thumbnail' content='https://christianparanas.vercel.app/me.png' />
+        <meta property='og:title' content='Christian Paranas | Web Developer' />
         <meta property='og:site_name' content='Christian Paranas' />
-        <meta
-          name='keywords'
-          content='Web developer, Frontend developer, Backend developer'
-        />{' '}
-        <meta
-          name='thumbnail'
-          content='https://christianparanas.vercel.app/me.png'
-        />
-        <meta property='og:type' content='website' />{' '}
-        <meta
-          property='og:description'
-          content='Hi, I am Christian Paranas a web development enthusiast that is armed with experience, learning, and passion.'
-        />
-        <meta
-          property='og:image'
-          content='https://christianparanas.vercel.app/me.png'
-        />
+        <meta name='keywords' content='Web developer, Frontend developer, Backend developer' />
+        <meta name='thumbnail' content='https://christianparanas.vercel.app/me.png' />
+        <meta property='og:type' content='website' />
+        <meta property='og:description' content='Hi, I am Christian Paranas a web development enthusiast that is armed with experience, learning, and passion.' />
+        <meta property='og:image' content='https://christianparanas.vercel.app/me.png' />
         <meta name='twitter:card' content='summary' />
-        <meta
-          name='twitter:title'
-          content='Christian Paranas | Web Developer'
-        />
-        <meta
-          name='twitter:description'
-          content='Hi, I am Christian Paranas a web development enthusiast that is armed with experience, learning, and passion.'
-        />
-        <meta
-          name='twitter:image'
-          content='https://christianparanas.vercel.app/me.png'
-        />
+        <meta name='twitter:title' content='Christian Paranas | Web Developer' />
+        <meta name='twitter:description' content='Hi, I am Christian Paranas a web development enthusiast that is armed with experience, learning, and passion.' />
+        <meta name='twitter:image' content='https://christianparanas.vercel.app/me.png' />
         <meta name='twitter:site' content='https://christianparanas.vercel.app' />
         <meta name='twitter:creator' content='@christianparams' />
         <meta name='author' content='Christian R. Paranas' />
         <meta name='robots' content='index,follow' />
         <link rel='canonical' href='https://christianparanas.vercel.app/' />
       </Head>
+
     <ThemeProvider defaultTheme="dark">
       <Layout>
         <Component {...pageProps} />

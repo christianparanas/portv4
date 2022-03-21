b4587964ca663790f9cf3c21c73e480bad6f13b1
kate <christiannparanas@gmail.com>
2022-03-21 21:41:29 +0800

    feat: prisma

---
+++ pages/guests/index.jsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useEffect } from 'react'
+
+import prisma from '../../lib/prisma';
+
+
+export default function Guests({ props }) {
+
+  useEffect(() => {
+  	console.log(props)
+
+  }, [])
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Guests | Christian Paranas</title>
+      </Head>
+
+      <main className="guests">
+
+        guests
+      </main>
+
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const entries = await prisma.guests.findMany({
+    orderBy: {
+      updatedAt: 'desc'
+    }
+  });
+
+  const fallbackData = entries.map((entry) => ({
+    id: entry.id.toString(),
+    body: entry.body,
+    created_by: entry.created_by.toString(),
+    updated_at: entry.updatedAt.toString()
+  }));
+
+  console.log(fallbackData)
+
+
+  return {
+    props: {
+    	fallbackData,
+    }
+  };
+};
---
+++ prisma/migrations/20220321131306_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "guests" (
+    "id" SERIAL NOT NULL,
+    "email" VARCHAR(256) NOT NULL,
+    "body" VARCHAR(500) NOT NULL,
+    "created_by" VARCHAR(256) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "guests_pkey" PRIMARY KEY ("id")
+);

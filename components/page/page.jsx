d8d64e9ca50738e942cfb184c72f482958232815
kate <christiannparanas@gmail.com>
2022-04-06 21:21:37 +0800

    feat: about pppp

--- components/page/page.jsx
+++ components/page/page.jsx
@@ -8,7 +8,6 @@ const footerLinks = [
   { name: "Home", url: "/" },
   { name: "Twitter", url: "https://twitter.com/chrisparams" },
   { name: "LinkedIn", url: "https://www.linkedin.com/in/christianparanas" },
-  { name: "Newsletter", url: "/newsletter" },
   { name: "About", url: "/about" },
   { name: "GitHub", url: "https://github.com/christianparanas" },
   { name: "RSS", url: "/feed.xml" },
@@ -16,6 +15,7 @@ const footerLinks = [
   { name: "Dribbble", url: "https://dribbble.com/chris" },
   { name: "Books", url: "/books" },
   { name: "Changelog", url: "/changelog" },
+  { name: "Newsletter", url: "/newsletter" }
 ];
 
 const Page = ({ children }) => (
--- pages/about.jsx
+++ pages/about.jsx
@@ -22,16 +22,41 @@ export default function About() {
             src={me}
             alt="Picture of me (Christian Paranas)"
             placeholder="blur"
-            width={450}
-            height={450}
+            width={320}
+            height={320}
             className={styles.image}
           />
         </div>
 
-        <div className="data">
+        <div className={styles.data}>
           <p>
-            Hi, I am Christian Paranas, a web developer currently living in
-            Tacloban City, Philippines.
+            Hi, I am Christian Paranas, an IT student from the Philippines. I
+            love spending my free time watching video tutorials and reading
+            articles about software development because I know it will give me
+            great benefits when the time comes. I don't what I'm going to put in
+            here, so, I will be right back haha.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+            magnam illum rerum laborum? Quaerat ratione, sapiente omnis
+            consequatur fugit deserunt eaque beatae voluptate, optio, possimus
+            voluptas ipsa iure magnam dolores debitis quos cumque aspernatur
+            error saepe consequuntur facilis consectetur minus soluta
+            laudantium. Numquam voluptates quidem a obcaecati deleniti alias
+            amet, saepe voluptate, sequi magnam ad est fuga, odit sapiente ipsam
+            consequuntur dolorum. Illo ratione cum quae temporibus fugiat
+            dolorem pariatur non ea maxime in dicta laudantium necessitatibus
+            enim, tempore sunt magni laborum cumque harum voluptate impedit
+            obcaecati. Modi error incidunt soluta corporis et, culpa, sit
+            nesciunt, reprehenderit explicabo cumque repellendus.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis
+            exercitationem quidem optio in soluta deleniti vel maiores ad harum
+            eaque numquam sapiente illo repudiandae expedita officia id
+            laudantium autem, sequi est hic ratione illum? Consequuntur possimus
+            mollitia voluptatum, fugit deserunt dolor dicta. Dolore voluptates
+            veritatis eum aliquid quia ab officiis.
           </p>
         </div>
       </main>

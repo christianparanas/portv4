c833a8b0ef3f49080c7f2a84536fb53bcfcf5888
kate <christiannparanas@gmail.com>
2022-05-24 14:22:15 +0800

    styles: background image

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,9 @@ import Services from "components/Services";
 export default function Home() {
   return (
     <Page>
+      <div className="hidden md:block fixed -z-50 -right-[100px] opacity-5">
+        <img src="/images/javascript.svg" alt="" className="w-[500px]" />
+      </div>
       <Hero
         title="Hi, I am Christian Paranas."
         description="I’m a web developer based in Tacloban City, Philippines. My current toolset includes React, Angular, Node, Laravel, and other various frameworks, libraries, and technologies related to them."

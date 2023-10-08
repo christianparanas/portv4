22f1038a1e09fed881f3b3465ebf13dec8f4e78a
Christian Paranas <dev.christianparanas@gmail.com>
2023-10-08 09:45:03 +0800

    update about info and fix image error

--- pages/about.jsx
+++ pages/about.jsx
@@ -19,7 +19,7 @@ export default function About() {
           <div className="grid md:grid-cols-[300px_1fr] gap-[30px]">
             <div className="w-[300px] mx-auto">
               <Image
-                src="https://avatars.githubusercontent.com/u/59472122?u=80920e8c7f13d0415c3a68ff7f36527bdd3d3003&v=4"
+                src="https://avatars.githubusercontent.com/u/59472122?v=4"
                 alt="Picture of me (Christian Paranas)"
                 placeholder="blur"
                 width={300}
@@ -29,15 +29,10 @@ export default function About() {
             </div>
             <span className="md:text-md mt-6 text-slate-900 dark:text-slate-50">
               <p className="mb-6">
-                Hello! I am Christian Paranas from Tacloban city,
-                Philippines. I am currently a college student, taking a
-                bachelor's degree in Information Technology at Eastern Visayas
-                State University.
+                Hello! I am Christian Paranas from Tacloban City, Philippines. I have recently graduated from Eastern Visayas State University with a bachelor's degree in Information Technology. 
               </p>
               <p>
-                If I have spare time, I do freelance work, like building
-                websites, software programs, photo editing, and other IT-related
-                tasks for my schoolmates and for other people who need my services.
+                In my spare time, I engage in freelance work, offering services such as website development, software programming, photo editing, and other IT-related tasks to assist my schoolmates and anyone else in need of my expertise.
               </p>
             </span>
           </div>
